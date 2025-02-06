6b14c3ea456ede37d586bd27db53e7978d27b91b
aqyakush <anton.yakushonok@gmail.com>
2025-02-06 21:22:33 +0200

    Remove unused pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './pages/Authentication/Login';
-import { TokenProvider } from './pages/Authentication/TokenContext';
-import Protected from './pages/Protected/protected';
+// import Login from './pages/Authentication/Login';
+// import { TokenProvider } from './pages/Authentication/TokenContext';
+// import Protected from './pages/Protected/protected';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import styled from 'styled-components';
@@ -25,25 +25,25 @@ const Main = styled.main`
 const App: React.FC = () => {
   return (
     <Router>
-      <TokenProvider>
+      {/* <TokenProvider> */}
         <AppWrapper>
           <Navigation />
           <Main>
             <Routes>
               {/* <Route path="/login" element={<Login />} /> */}
               <Route path="/" element={<Home />} />
-              <Route path="/protected" element={<Protected/> } />
+              {/* <Route path="/protected" element={<Protected/> } /> */}
               <Route path="*" element={<Navigate to="/not-protected" />} />
               <Route path="/preorder" element={<Preorder />} />
               <Route path="/subscription" element={<Subscription />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/our-story" element={<OurStory />} />
-              <Route path="/login" element={<Login />} />
+              {/* <Route path="/login" element={<Login />} /> */}
             </Routes>
           </Main>
           <Footer />
       </AppWrapper>
-    </TokenProvider>
+    {/* </TokenProvider> */}
     </Router>
     
 
--- src/pages/Protected/protected.tsx
+++ src/pages/Protected/protected.tsx
@@ -1,42 +1,42 @@
-// src/Protected.tsx
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { TokenContext } from '../Authentication/TokenContext';
-import { useNavigate } from 'react-router-dom';
+// // src/Protected.tsx
+// import React, { useContext, useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { TokenContext } from '../Authentication/TokenContext';
+// import { useNavigate } from 'react-router-dom';
 
-interface ProtectedProps {
-}
+// interface ProtectedProps {
+// }
 
-const Protected: React.FC<ProtectedProps> = () => {
-  const [data, setData] = useState<any>(null);
-  const { token, logout: handleLogout } = useContext(TokenContext)!;
-  const navigate = useNavigate();
+// const Protected: React.FC<ProtectedProps> = () => {
+//   const [data, setData] = useState<any>(null);
+//   const { token, logout: handleLogout } = useContext(TokenContext)!;
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:8001/api/protected/', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        console.log(response.data);
-        setData(response.data);
-      } catch (error) {
-        navigate('/login');
-      }
-    }
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('http://127.0.0.1:8001/api/protected/', {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+//         console.log(response.data);
+//         setData(response.data);
+//       } catch (error) {
+//         navigate('/login');
+//       }
+//     }
 
-    fetchData();
-  }, [token]);
+//     fetchData();
+//   }, [token]);
 
-  return (
-    <div>
-      <h2>Protected Data</h2>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Protected Data</h2>
+//       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+//       <button onClick={handleLogout}>Logout</button>
+//     </div>
+//   );
+// };
 
-export default Protected;
+// export default Protected;
