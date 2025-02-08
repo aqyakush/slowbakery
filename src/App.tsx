37b574ab1e52fcfbc5a3cb76a1e8ee35466fadc4
aqyakush <anton.yakushonok@gmail.com>
2025-02-08 10:17:15 +0200

    Fix form and remove unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import styled from 'styled-components';
