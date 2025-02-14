ee58798231140045e0818e22f112dbb8b8d444ff
Anton Y. <aqyakush@users.noreply.github.com>
2025-02-14 19:25:41 +0200

    Update index.tsx

--- src/pages/OurStory/index.tsx
+++ src/pages/OurStory/index.tsx
@@ -6,7 +6,7 @@ export default function OurStory() {
       <Title>Our Story</Title>
       <Content>
         <p>
-            Let me introduce myself. My name is Soeun, and I’ve been baking since I moved to Finland. I discovered sourdough baking about a year ago, and I’ve been hooked ever since. One of the main reasons I started baking was because of my autoimmune conditions, which make my body extra sensitive to chemicals, sugar, and other additives. I wanted to create healthy baked goods that both my body and I could enjoy—and now I’m thrilled to share them with you!
+            Let me introduce myself. My name is XXX. I discovered sourdough baking about a year ago, and I’ve been hooked ever since.
         </p>
       </Content>
     </PageWrapper>
