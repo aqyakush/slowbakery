96aa85d180bf9e90ab5b400af3c0400c72beb004
aqyakush <anton.yakushonok@gmail.com>
2025-02-20 20:33:50 +0200

    change port for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/slowbakery/',
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+	const isProduction = command === 'build';
+  
+	return {
+	  base: isProduction ? '/slowbakery/' : '', // Conditionally set the base URL
+	  plugins: [react()],
+	  server: {
+		port: 8000
+	  }
+	};
+  });
