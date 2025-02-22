df435c6fb92ea8447a486f34fd75e5f87d6c12cb
aqyakush <anton.yakushonok@gmail.com>
2025-02-22 09:31:23 +0200

    Update navigation layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Link } from 'react-router-dom';
+import {  Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { FaHome, FaBookOpen, FaBoxOpen, FaBell, FaShoppingCart, FaEnvelope} from 'react-icons/fa';
 import { useShoppingCart } from '../context/ShoppingCartContext';
@@ -7,18 +7,14 @@ import { useTranslation } from 'react-i18next';
 
 
 const NavigationWrapper = styled.nav`
-  background-color: #fef3c7;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  width: 100%;
-`;
-
-const NavigationContent = styled.div`
-  margin: 0 auto;
-  padding: 1.5rem 1rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+  background-color: #fef3c7;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  padding: 1.5rem 1rem;
+  overflow: hidden;
+`;
 
 const Logo = styled(Link)`
   display: flex;
@@ -35,24 +31,35 @@ const LogoText = styled.span`
 
 const NavLinks = styled.ul`
   display: flex;
+  justify-content: center;
+  align-items: center;
   gap: 1rem;
   list-style: none;
   margin: 0;
   padding: 0;
+  flex: 1;
 `
 
-const NavLink = styled(Link)`
+const StyledNavLink = styled(NavLink)` // Renamed to avoid conflict with Link
   color: #92400e;
   text-decoration: none;
   &:hover {
     color: #b45309;
   }
-`
+`;
 
 const IconTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 5px;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  white-space: nowrap;
 `;
 
 const CartIconWrapper = styled.div`
@@ -105,72 +112,68 @@ const Navigation: React.FC = () => {
 
   return (
     <NavigationWrapper>
-      <NavigationContent>
-          <Logo to="/">
-            <LogoText>Slow Bakery</LogoText>
-          </Logo>
-          <NavLinks>
-            <li>
-              <NavLink to="/our-story">
+        <Logo to="/">
+          <LogoText>Slow Bakery</LogoText>
+        </Logo>
+        <NavLinks>
+          <li>
+            <StyledNavLink to="/our-story">
+            <IconTextWrapper>
+              <FaBookOpen />
+              <span>{t('ourStory')}</span>
+              </IconTextWrapper>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/preorder">
+            <IconTextWrapper>
+              <FaBoxOpen />
+              <span>{t('preorder')}</span>
+              </IconTextWrapper>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/subscription">
+            <IconTextWrapper>
+              <FaBell />
+              <span>{t('subscription')}</span>
+              </IconTextWrapper>
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/contact">
               <IconTextWrapper>
-                <FaBookOpen />
-                <span>{t('ourStory')}</span>
-                </IconTextWrapper>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/preorder">
-              <IconTextWrapper>
-                <FaBoxOpen />
-                <span>{t('preorder')}</span>
-                </IconTextWrapper>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/subscription">
-              <IconTextWrapper>
-                <FaBell />
-                <span>{t('subscription')}</span>
-                </IconTextWrapper>
-              </NavLink>
-            </li>
-            {items.length > 0 && (
-              <li>
-                <NavLink to="/shopping-cart">
-                <CartIconWrapper>
-                  <FaShoppingCart size={24} />
-                  {items.length > 0 && <CartBubble>{items.length}</CartBubble>}
-                </CartIconWrapper>
-                </NavLink>
-              </li>
-            )}
-            <li>
-              <NavLink to="/contact">
-                <IconTextWrapper>
-                  <FaEnvelope />
-                  <span>{t('contact')}</span>
-                </IconTextWrapper>
-              </NavLink>
-            </li>
-            <li>
-            <LanguageSwitcher>
-              <LanguageButton
-                onClick={() => changeLanguage('fi')}
-                selected={i18n.language === 'fi'}
-              >
-                FI
-              </LanguageButton>
-              <span>/</span>
-              <LanguageButton
-                onClick={() => changeLanguage('en')}
-                selected={i18n.language === 'en'}
-              >
-                EN
-              </LanguageButton>
-            </LanguageSwitcher>
+                <FaEnvelope />
+                <span>{t('contact')}</span>
+              </IconTextWrapper>
+            </StyledNavLink>
           </li>
           </NavLinks>
-      </NavigationContent>
+          <RightSection>
+          {items.length > 0 && (
+              <StyledNavLink to="/shopping-cart">
+              <CartIconWrapper>
+                <FaShoppingCart size={24} />
+                {items.length > 0 && <CartBubble>{items.length}</CartBubble>}
+              </CartIconWrapper>
+              </StyledNavLink>
+          )}
+          <LanguageSwitcher>
+            <LanguageButton
+              onClick={() => changeLanguage('fi')}
+              selected={i18n.language === 'fi'}
+            >
+              FI
+            </LanguageButton>
+            <span>/</span>
+            <LanguageButton
+              onClick={() => changeLanguage('en')}
+              selected={i18n.language === 'en'}
+            >
+              EN
+            </LanguageButton>
+          </LanguageSwitcher>
+      </RightSection>
     </NavigationWrapper>
   );
 };
